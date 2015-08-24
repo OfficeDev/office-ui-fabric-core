9dc74edb6af90ab390547488f7d9dfe4209f7a37
Peter Jahn <pejahn@microsoft.com>
2015-08-24 12:12:36 -0700

    Add compiled files

---
+++ dist/components/ListItem/Jquery.ListItem.js
@@ -0,0 +1,25 @@
+/**
+ * List Item Plugin
+ *
+ * Adds basic demonstration functionality to .ms-ListItem components.
+ *
+ * @param  {jQuery Object}  One or more .ms-ListItem components
+ * @return {jQuery Object}  The same components (allows for chaining)
+ */
+(function ($) {
+  $.fn.ListItem = function () {
+
+    /** Go through each panel we've been given. */
+    return this.each(function () {
+
+      var $listItem = $(this);
+
+      /** Detect clicks on selectable list items. */
+      $listItem.on('click', '.js-toggleSelection', function(event) {
+        $(this).parents('.ms-ListItem').toggleClass('is-selected');
+      });
+
+    });
+
+  };
+})(jQuery);

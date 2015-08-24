ba57c928a7cebd1c426e7949a087760f4219b3f1
John Miller <rubixws@gmail.com>
2015-08-24 12:07:05 -0700

    - Removed unnecessary white space. - Added spaces before ()'s - Updated fabric build all message so its dependent on both fabric and component build tasks. - Added a default task just for building and not watching.

--- gulp/fabric.js
+++ gulp/fabric.js
@@ -38,7 +38,6 @@ var paths = {
     srcPath: srcPath,
     componentsPath : 'src/components',
     lessPath: srcPath + '/less',
-    distCompsPath : distPath + '/components',
     templatePath : srcPath + '/templates'
 };
 
@@ -59,14 +58,12 @@ var bannerTemplate = ['/**',
       ' **/',
       ''].join('\n');
 
-
 // Confgure data objects to pass into banner plugin.
 var bannerData = {
     pkg : pkg,
     date: date,
     monthNames: monthNames
 }
-
 
 // Emit the end of the event so further pipes don't continue working
 // on pipes that have bad data/files in it. Essentially, errors shouldn't cause
@@ -77,26 +74,21 @@ var onGulpError = function (error) {
 };
 
 // Success message
-var generateSuccess = function(message, showTip) {
-
+var generateSuccess = function (message, showTip) {
     var spacing = "\r\n";
     var spaceDashes = colors.rainbow("---------------------------------------------------");
-    
     if(showTip == true) {
         var tipsMessage = colors.gray("TIP: In order to test changes to fabric source, you can view demos for each component inside our /samples directory.") + spacing;
     } else {
         var tipsMessage = "";
     }
-
     var consoleText = colors.green("Fabric Message: ") + colors.cyan(message);
     var completeMessage = spaceDashes + spacing + spacing + consoleText + spacing + tipsMessage + spacing + spaceDashes;
-
     return completeMessage;
-
 }
 
 // Helper for retrieving folders
-var getFolders = function(dir) {
+var getFolders = function (dir) {
     return fs.readdirSync(dir)
     .filter(function(file) {
         return fs.statSync(path.join(dir, file)).isDirectory();
@@ -108,14 +100,13 @@ var componentsFolders = getFolders(paths.componentsPath);
 var catalogContents = "";
 
 // Clean out the distribution folder.
-gulp.task('clean-fabric', function() {
+gulp.task('clean-fabric', function () {
     return del.sync([paths.distLess, paths.distCSS]);
 });
 
-gulp.task('clean-components', function() {
+gulp.task('clean-components', function () {
     return del.sync([paths.distComponents]);
 });
-
 
 //
 // Tasks for building Fabric for distribution.
@@ -128,7 +119,7 @@ gulp.task('copy-fabric', ['clean-fabric'], function () {
         .pipe(gulp.dest(paths.distPath + '/less'));
 });
 
-gulp.task('copy-components', ['clean-components'], function() {
+gulp.task('copy-components', ['clean-components'], function () {
     // Copy all Components files.
     return gulp.src('src/components/**')
         .pipe(gulp.dest(paths.distPath + '/components'));
@@ -137,17 +128,14 @@ gulp.task('copy-components', ['clean-components'], function() {
 // All Copy tasks
 gulp.task('copy', ['copy-fabric', 'copy-components']);
 
-
 // Build LESS files for core Fabric and Components into LTR and RTL CSS files.
-gulp.task('fabric-less', ['clean-fabric'], function() {
-
+gulp.task('fabric-less', ['clean-fabric'], function () {
     // Confgure data objects to pass into banner plugin.
     var bannerData = {
         pkg : pkg,
         date: date,
         monthNames: monthNames
     }
-
     // Baseline set of tasks for building Fabric CSS.
     var _fabricBase = function() {
         return gulp.src(['src/less/fabric.less'])
@@ -163,7 +151,6 @@ gulp.task('fabric-less', ['clean-fabric'], function() {
             }))
                 .on('error', onGulpError);
     }
-
     // Build full and minified Fabric CSS.
     var fabric = _fabricBase()
             .pipe(cssbeautify())
@@ -178,7 +165,6 @@ gulp.task('fabric-less', ['clean-fabric'], function() {
                 .on('error', onGulpError)
             .pipe(gulp.dest(paths.distPath + '/css/'))
                 .on('error', onGulpError);
-
     // Build full and minified Fabric RTL CSS.
     var fabricRtl = gulp.src('src/less/fabric.rtl.less')
             .pipe(less())
@@ -206,15 +192,12 @@ gulp.task('fabric-less', ['clean-fabric'], function() {
                 .on('error', onGulpError)
             .pipe(gulp.dest(paths.distPath + '/css/'))
                 .on('error', onGulpError);
-  
-
     // Merge all current streams into one.
     return mergeStream(fabric, fabricRtl);
 });
 
 // Components construction
-gulp.task('components-less', ['clean-components'], function() {
-
+gulp.task('components-less', ['clean-components'], function () {
     var _componentsBase = function() {
         return gulp.src('src/less/fabric.components.less')
             .pipe(less())
@@ -227,7 +210,6 @@ gulp.task('components-less', ['clean-components'], function() {
             }))
             .on('error', onGulpError);
     }
-
     // Build full and minified Fabric Components CSS.
     var components = _componentsBase()
         .pipe(rename('fabric.components.css'))
@@ -244,7 +226,6 @@ gulp.task('components-less', ['clean-components'], function() {
             .on('error', onGulpError)
         .pipe(gulp.dest(paths.distPath + '/css/'))
             .on('error', onGulpError);
-
       // Build full and minified Fabric Components CSS for each Component.
     var indComponents = componentsFolders.map(function(folder) {
         var manifest = JSON.parse(fs.readFileSync(paths.componentsPath + '/' +  folder + '/' +  folder + '.json'));
@@ -271,17 +252,15 @@ gulp.task('components-less', ['clean-components'], function() {
                 .on('error', onGulpError)
             .pipe(csscomb())
                 .on('error', onGulpError)
-            .pipe(gulp.dest(paths.distCompsPath + '/' + folder))
+            .pipe(gulp.dest(paths.distComponents + '/' + folder))
                 .on('error', onGulpError)
             .pipe(rename(folder + '.min.css'))
                 .on('error', onGulpError)
             .pipe(cssMinify())
                 .on('error', onGulpError)
-            .pipe(gulp.dest(paths.distCompsPath  + '/' + folder))
+            .pipe(gulp.dest(paths.distComponents  + '/' + folder))
                 .on('error', onGulpError);
     });
-
-
     // Build full and minified Fabric Components RTL CSS.
     var componentsRtl = _componentsBase()
             .pipe(flipper())
@@ -300,16 +279,15 @@ gulp.task('components-less', ['clean-components'], function() {
                 .on('error', onGulpError)
             .pipe(gulp.dest(paths.distPath + '/css/'))
                 .on('error', onGulpError);
-
     return mergeStream(components, componentsRtl, indComponents);
 });
 
-gulp.task('build-component-data', folders(paths.componentsPath, function(folder) {
+gulp.task('build-component-data', folders(paths.componentsPath, function (folder) {
     return gulp.src(paths.componentsPath + '/' +  folder + '/*.html')
             .on('error', onGulpError)
         .pipe(concat(folder + '.html'))
             .on('error', onGulpError)
-        .pipe(tap(function(file) {
+        .pipe(tap(function (file) {
             storedFiles[folder] = file.contents.toString();
             var curString = storedFiles[folder];
             curString = JSON.stringify(curString);
@@ -321,7 +299,7 @@ gulp.task('build-component-data', folders(paths.componentsPath, function(folder)
             .on('error', onGulpError);
 }));
 
-gulp.task('build-component-examples', ['build-component-data'], folders(paths.componentsPath, function(folder){
+gulp.task('build-component-examples', ['build-component-data'], folders(paths.componentsPath, function (folder){
     return gulp.src(paths.templatePath + '/'+ 'individual-component-example.html')
         .on('error', onGulpError)
     .pipe(data(function () {
@@ -332,10 +310,9 @@ gulp.task('build-component-examples', ['build-component-data'], folders(paths.co
         .on('error', onGulpError)
     .pipe(rename(folder + '-demos.html'))
         .on('error', onGulpError)
-    .pipe(gulp.dest(paths.distCompsPath + '/' +  folder))
+    .pipe(gulp.dest(paths.distComponents + '/' +  folder))
         .on('error', onGulpError);
 }));
-
 
 // Roll up static resource building
 gulp.task('build-fabric', ['clean-fabric', 'copy-fabric', 'fabric-less']);
@@ -343,66 +320,62 @@ gulp.task('build-fabric', ['clean-fabric', 'copy-fabric', 'fabric-less']);
 // Build for fabric component demos
 gulp.task('build-components', ['clean-components', 'copy-components', 'components-less', 'build-component-data', 'build-component-examples']);
 
-
 // Fabric success messages
-gulp.task('fabric-finished', ['build-fabric'], function() {
+gulp.task('fabric-finished', ['build-fabric'], function () {
     console.log(generateSuccess('Fabric core-build complete, you may now celebrate and dance!', true));
 });
 
-gulp.task('fabric-updated', ['build-fabric'], function() {
+gulp.task('fabric-updated', ['build-fabric'], function () {
     console.log(generateSuccess(' Fabric updated succesfully', false));
 });
 
-gulp.task('components-finished', ['build-components'], function() {
+gulp.task('components-finished', ['build-components'], function () {
     console.log(generateSuccess(' Components build was succesfull! Yay!', true));
 });
 
-gulp.task('components-updated', ['build-components'], function() {
+gulp.task('components-updated', ['build-components'], function () {
     console.log(generateSuccess(' Components updated succesfully! Yay!'));
 });
 
-gulp.task('fabric-all-finished', ['build-fabric'], function() {
+gulp.task('fabric-all-finished', ['build-fabric', 'build-components'], function () {
     console.log(generateSuccess('All Fabric parts built succesfully, you may now celebrate and dance!', true));
 });
 
-gulp.task('fabric-all-updated', ['build-fabric'], function() {
+gulp.task('fabric-all-updated', ['build-fabric', 'build-components'], function () {
     console.log(generateSuccess('All Fabric parts updated succesfully! Yay!', true));
 });
 
-
 // Fabric watch tasks
-gulp.task('watch:components', ['build-components', 'components-finished'], function() {
-    return gulp.watch(paths.componentsPath + '/**/*', batch(function(events, done) {
+gulp.task('watch:components', ['build-components', 'components-finished'], function () {
+    return gulp.watch(paths.componentsPath + '/**/*', batch(function (events, done) {
         runSequence('build-components', 'components-updated', done);
     }));
 });
 
 // Watch and build Fabric when sources change.
-gulp.task('watch:fabric', ['build-fabric', 'fabric-finished'], function() {
-    return gulp.watch(paths.lessPath + '/**/*', batch(function(events, done) {
+gulp.task('watch:fabric', ['build-fabric', 'fabric-finished'], function () {
+    return gulp.watch(paths.lessPath + '/**/*', batch(function (events, done) {
         runSequence('build-fabric', 'fabric-updated', done);
     }));
 });
 
 // Watch components and fabric at the same time but build seperately.
-gulp.task('watch:separately', ['build-fabric', 'build-components', 'fabric-finished'], function() {
-
-    gulp.watch(paths.lessPath + '/**/*', batch(function(events, done) {
+gulp.task('watch:separately', ['build-fabric', 'build-components', 'fabric-finished'], function () {
+    gulp.watch(paths.lessPath + '/**/*', batch(function (events, done) {
         runSequence('build-fabric', 'fabric-updated', done);
     }));
-
-    gulp.watch(paths.componentsPath + '/**/*', batch(function(events, done) {
+    gulp.watch(paths.componentsPath + '/**/*', batch(function (events, done) {
         runSequence('build-components', 'components-updated', done);
     }));
-
 });
 
 // Watch and build Fabric when sources change.
-gulp.task('watch', ['build-fabric', 'build-components', 'fabric-all-finished'], function() {
-
-    gulp.watch(paths.srcPath + '/**/*', batch(function(events, done) {
+gulp.task('watch', ['build-fabric', 'build-components', 'fabric-all-finished'], function () {
+    gulp.watch(paths.srcPath + '/**/*', batch(function (events, done) {
         runSequence('build-fabric', 'build-components', 'fabric-all-updated', done);
     }));
-
 });
+
+// Default Build Task
+gulp.task('build', ['build-fabric', 'build-components', 'fabric-all-finished']);
 

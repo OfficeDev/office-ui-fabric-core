91516825d2de866c94d2d8319982786199e7c225
Jonah Kirangi <jonahkirangi@gmail.com>
2015-10-14 16:49:14 -0700

    Only display a maximum of 5 people

--- src/components/Facepile/Jquery.Facepile.js
+++ src/components/Facepile/Jquery.Facepile.js
@@ -14,21 +14,31 @@
     /** Iterate through each Facepile provided. */
     return this.each(function () {
       
-      var $facePile = $(this);  
-      var $n = $(".ms-Persona--xs").length;
+      var $facePile = $(this);        
+      var $n = $(".ms-Facepile-members-withOverflow > button").length;
 
       /** Add member when button clicked (No Overflow) */
       $facePile.on('click', '.ms-Facepile-circleFilled-noOverflow', function(event) {                
-        $(this).parent().next().children(':last').clone().appendTo(".ms-Facepile-members-noOverflow");        
+        $(this).parent().next().children(':last').clone().prependTo(".ms-Facepile-members-noOverflow");        
       });
 
       /** Add member when button clicked (With Overflow) */
-      $facePile.on('click', '.ms-Facepile-circleFilled-withOverflow', function(event) {                
-        $(this).parent().next().children(':first').clone().appendTo(".ms-Facepile-members-withOverflow");  
+      $facePile.on('click', '.ms-Facepile-circleFilled-withOverflow', function(event) {
+        $(this).parent().next().children(':first').clone().prependTo(".ms-Facepile-members-withOverflow");  
 
-        // Increment number of members by one
+        // Increment members by one
         $n += 1;
-        $(".ms-Facepile-overflow").text("+" + $n);       
+        $(".ms-Facepile-overflow").text("+" + $n);
+
+        // Display a maxiumum of 6 members
+        $(".ms-Facepile-members-withOverflow").children(":gt(4)").hide();
+
+        // Display chevronDown while members are 5 or less
+        if ($n > 4) {
+          $("span.ms-Facepile-overflow").removeClass("is-hidden");
+          $("i.ms-Facepile-chevronThickDown").addClass("is-hidden");
+        }
+
       });
 
       // Display member count on page load

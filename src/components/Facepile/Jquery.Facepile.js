e5846f63ff04b086c2cee088bf39d275c92c2a10
Phil Worthington <a-phwort@microsoft.com>
2015-10-23 16:33:33 -0700

    Refactor facepile template and less. Update casing of facepile name

--- src/components/Facepile/Jquery.Facepile.js
+++ src/components/Facepile/Jquery.Facepile.js
@@ -3,47 +3,47 @@
 /**
  * Facepile Plugin
  *
- * Adds basic demonstration functionality to .ms-Facepile components.
+ * Adds basic demonstration functionality to .ms-FacePile components.
  *
- * @param  {jQuery Object}  One or more .ms-Facepile components
+ * @param  {jQuery Object}  One or more .ms-FacePile components
  * @return {jQuery Object}  The same components (allows for chaining)
  */
 (function ($) {
-  $.fn.Facepile = function () {
+  $.fn.FacePile = function () {
 
     /** Iterate through each Facepile provided. */
     return this.each(function () {
       
       var $facePile = $(this);        
-      var $n = $(".ms-Facepile-members-withOverflow > button").length;      
+      var $n = $(".ms-FacePile-members-withOverflow > button").length;      
 
       /** Add person when button clicked */
       $facePile.on("click", ".js-addPerson", function(event) {
-        $(this).parent().next().children(":first").clone().prependTo(".ms-Facepile-members-withOverflow");  
+        $(this).parent().next().children(":first").clone().prependTo(".ms-FacePile-members-withOverflow");  
 
         /** Increment person count by one */
         $n += 1;
-        $(".ms-Facepile-overflow").text("+" + $n);
+        $(".ms-FacePile-overflow").text("+" + $n);
 
         /** Display a maxiumum of 6 people */
-        $(".ms-Facepile-members-withOverflow").children(":gt(4)").hide();
+        $(".ms-FacePile-members-withOverflow").children(":gt(4)").hide();
 
         /** Display counter when 5 people are present */
         if ($n > 4) {
-          $("span.ms-Facepile-overflow").removeClass("is-hidden");
-          $("i.ms-Facepile-chevronThickDown").addClass("is-hidden");
+          $("span.ms-FacePile-overflow").removeClass("is-hidden");
+          $("i.ms-FacePile-chevronThickDown").addClass("is-hidden");
         }
 
         /** Re-position counter when double digits reached */
         if ($n > 9) {
-          $(".ms-Facepile-overflow").addClass("ms-Facepile-doubleDigits");
+          $(".ms-FacePile-overflow").addClass("ms-FacePile-doubleDigits");
         }
 
       });
 
       /** Display person count on page load */
       $(document).ready(function() {
-        $(".ms-Facepile-overflow").text("+" + $n);
+        $(".ms-FacePile-overflow").text("+" + $n);
       });  
 
     });

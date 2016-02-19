be4238d7246fe10cee4dffc3a99117fb06585c25
Peter Jahn <Peter Jahn>
2016-02-19 11:39:19 -0800

    Whitespace adjustments to just the bundles section

--- gulp/modules/Config.js
+++ gulp/modules/Config.js
@@ -67,39 +67,39 @@ var Config = function() {
   //Errors
   this.genericBuildError = "Hmm, something went wrong in the build... Here is the error dump";
 	this.bundlesConfig = {
-		"bundles": [
-				{
-					"name": "fabric-full",
-					"description": "A bundle containing all of Fabric's core and Component CSS.",
-					"excludes": [],
-					"options": {
-						// Log helpful messages about the bundles being built. 
-						"verbose": true,
+    "bundles": [
+        {
+          "name": "fabric-full",
+          "description": "A bundle containing all of Fabric's core and Component CSS.",
+          "excludes": [],
+          "options": {
+            // Log helpful messages about the bundles being built. 
+            "verbose": true,
 
-						// Log warnings about the bundles being built. 
-						"logWarnings": false
-					}
-				},
-				{
-					"name": "custom-bundle",
-					"description": "A custom bundle including a handful of modules.",
-					"includes": [
-						"_Fabric.Color.Variables",
-						"_Fabric.Color.Mixins",
-						"_Fabric.Typography.Variables",
-						"_Fabric.Typography",
-						"_Fabric.Typography.Fonts",
-						"_Fabric.Typography.Languageoverrides",
-						"_Fabric.Utilities",
-						"Button",
-						"PersonaCard"
-					],
-					"options": {
-						"verbose": true,
-						"logWarnings": false
-					}
-				}
-			]    
+            // Log warnings about the bundles being built. 
+            "logWarnings": false
+          }
+        },
+        {
+          "name": "custom-bundle",
+          "description": "A custom bundle including a handful of modules.",
+          "includes": [
+            "_Fabric.Color.Variables",
+            "_Fabric.Color.Mixins",
+            "_Fabric.Typography.Variables",
+            "_Fabric.Typography",
+            "_Fabric.Typography.Fonts",
+            "_Fabric.Typography.Languageoverrides",
+            "_Fabric.Utilities",
+            "Button",
+            "PersonaCard"
+          ],
+          "options": {
+            "verbose": true,
+            "logWarnings": false
+          }
+        }
+      ]    
 	}
 };
 

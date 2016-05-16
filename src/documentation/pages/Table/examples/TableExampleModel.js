c7bc966d53a5c9a9fa15165dc358785803bb70b9
Mike Wheaton <mike@mikewheaton.ca>
2016-05-16 16:20:44 -0700

    Add example templates and models for Table

---
+++ src/documentation/pages/Table/examples/TableExampleModel.js
@@ -0,0 +1,156 @@
+var TableExampleModel = {
+  "headers": [
+    {
+      "rowModifier": "rowCheck"
+    },
+    {
+      "value": "Location"
+    },
+    {
+      "value": "Modified"
+    },
+    {
+      "value": "Type"
+    },
+    {
+      "value": "File Name"
+    }
+  ],
+  "rows": [
+    {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    }
+  ]
+}
+
+module.exports = TableExampleModel;
---
+++ src/documentation/pages/Table/examples/TableFixedExampleModel.js
@@ -0,0 +1,157 @@
+var TableFixedExampleModel = {
+  "modifier": "fixed",
+  "headers": [
+    {
+      "rowModifier": "rowCheck"
+    },
+    {
+      "value": "Location"
+    },
+    {
+      "value": "Modified"
+    },
+    {
+      "value": "Type"
+    },
+    {
+      "value": "File Name"
+    }
+  ],
+  "rows": [
+    {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    },
+      {
+      "columns": [
+        {
+          "rowModifier": "rowCheck"
+        },
+        {
+          "value": "Location"
+        },
+        {
+          "value": "Modified"
+        },
+        {
+          "value": "Type"
+        },
+        {
+          "value": "File Name"
+        }
+      ]
+    }
+  ]
+}
+
+module.exports = TableFixedExampleModel;

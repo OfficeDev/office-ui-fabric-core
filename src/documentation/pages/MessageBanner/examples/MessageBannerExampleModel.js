7596ec1e5f7c11aa0a653ddeecaa287c935db1da
William Do <willdo@microsoft.com>
2016-05-17 17:30:09 -0700

    Finished MessageBanner.

--- src/documentation/pages/MessageBanner/examples/MessageBannerExampleModel.js
+++ src/documentation/pages/MessageBanner/examples/MessageBannerExampleModel.js
@@ -1,6 +1,15 @@
-var MessageBAnnerExampleModel = {
-  baseClass: "ms-MessageBanner",
-  message: ""
+var MessageBannerExampleModel = {
+  "props": {
+    "message": "You've reached your total storage on OneDrive. Please upgrade your storage plan if you need more storage.",
+    "button":  {
+      "name": "Button",
+      "props":  {
+        "label": "Get More Storage",
+        "modifier": "primary",
+        "tag": "button"
+      }
+    }
+  }
 }
 
-module.exports = MessageBAnnerExampleModel;
+module.exports = MessageBannerExampleModel;

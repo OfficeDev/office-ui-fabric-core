8080da520e402bcb0431b262b712827054eaf1f2
Phil Worthington <a-phwort@microsoft.com>
2016-05-23 16:31:27 -0700

    Update selectResult to close contextualmenu after selecting

--- src/components/PeoplePicker/PeoplePicker.ts
+++ src/components/PeoplePicker/PeoplePicker.ts
@@ -18,6 +18,7 @@ namespace fabric {
     private _container: Element;
     private _contextualHostView: ContextualHost;
     private _peoplePickerMenu: Element;
+    private _peoplePickerSearch: Element;
     private _peoplePickerResults: NodeListOf<Element>;
 
     /**
@@ -27,9 +28,10 @@ namespace fabric {
      */
     constructor(container: Element) {
       this._container = container;
-      this._assignClicks();
       this._peoplePickerMenu = this._container.querySelector(".ms-PeoplePicker-results");
-      this._peoplePickerResults = this._peoplePickerMenu.querySelectorAll(".ms-PeoplePicker-result");
+      this._peoplePickerSearch = this._container.querySelector(".ms-TextField-field");
+      this._peoplePickerResults = this._peoplePickerMenu.querySelectorAll(".ms-PeoplePicker-searchBox");
+      this._assignClicks();
 
       if (this._peoplePickerMenu) {
         this._peoplePickerMenu.setAttribute("style", "display: none;");
@@ -73,17 +75,28 @@ namespace fabric {
       let tokenResult: Element = <Element>currentResult.cloneNode(true);
       let searchBox = this._container.querySelector(".ms-PeoplePicker-searchBox");
       let textField = searchBox.querySelector(".ms-TextField");
+      let openHost = document.querySelector(".ms-ContextualHost.is-open");
 
+      // Add token classes to persona
       tokenResult.classList.add("ms-Persona--token", "ms-PeoplePicker-persona");
+
+      // Add the remove button to the token
       this._addRemoveBtn(tokenResult);
 
+      // Use persona xs variant for token
       if (tokenResult.classList.contains("ms-Persona--sm")) {
         tokenResult.classList.remove("ms-Persona--sm");
         tokenResult.classList.add("ms-Persona--xs");
       }
 
+      // Add removeToken event to actionIcon
       tokenResult.querySelector(".ms-Persona-actionIcon").addEventListener("click", this._removeToken.bind(this), true);
+
+      // Prepend the token before the search field
       searchBox.insertBefore(tokenResult, textField);
+
+      // Close the open contextual host
+      openHost.classList.remove("is-open");
     }
 
     private _findPersona(childObj: Element) {
@@ -113,9 +126,9 @@ namespace fabric {
     }
 
     private _assignClicks() {
-      this._container.addEventListener("click", this._clickHandler.bind(this), true);
-      this._container.addEventListener("focus", this._clickHandler.bind(this), true);
-      this._container.addEventListener("keyup", this._clickHandler.bind(this), true);
+      this._peoplePickerSearch.addEventListener("click", this._clickHandler.bind(this), true);
+      this._peoplePickerSearch.addEventListener("focus", this._clickHandler.bind(this), true);
+      this._peoplePickerSearch.addEventListener("keyup", this._clickHandler.bind(this), true);
     }
   }
 }

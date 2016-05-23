72489b452eb98b2f7ebd670a133d1a138e04ae15
Phil Worthington <a-phwort@microsoft.com>
2016-05-23 13:26:47 -0700

    Add removeBtn to tokens. Update selectResult so that items are cloned

--- src/components/PeoplePicker/PeoplePicker.ts
+++ src/components/PeoplePicker/PeoplePicker.ts
@@ -29,13 +29,16 @@ namespace fabric {
       this._container = container;
       this._assignClicks();
       this._peoplePickerMenu = this._container.querySelector(".ms-PeoplePicker-results");
+      this._peoplePickerResults = this._peoplePickerMenu.querySelectorAll(".ms-PeoplePicker-result");
 
       if (this._peoplePickerMenu) {
         this._peoplePickerMenu.setAttribute("style", "display: none;");
       }
     }
 
-    private _createModalHost() {
+    private _createModalHost(e) {
+      e.stopPropagation();
+
       this._peoplePickerMenu.setAttribute("style", "display: block;");
       this._contextualHostView = new fabric.ContextualHost(
         <HTMLElement>this._peoplePickerMenu,
@@ -46,7 +49,7 @@ namespace fabric {
     }
 
     private _clickHandler(e) {
-      this._createModalHost();
+      this._createModalHost(e);
 
       this._peoplePickerResults = this._peoplePickerMenu.querySelectorAll(".ms-PeoplePicker-result");
 
@@ -56,12 +59,17 @@ namespace fabric {
     }
 
     private _selectResult(e) {
-      // let currentResult = this._findPersona(e.target);
-      let tokenResult = this._findPersona(e.target);
-      // let tokenResult: Element = <Element>currentResult.cloneNode(true);
+      e.stopPropagation();
+
+      let currentResult = this._findPersona(e.target);
+      // let tokenResult = this._findPersona(e.target);
+      let tokenResult: Element = <Element>currentResult.cloneNode(true);
       let searchBox = this._container.querySelector(".ms-PeoplePicker-searchBox");
       let textField = searchBox.querySelector(".ms-TextField");
+
       tokenResult.classList.add("ms-Persona--token", "ms-PeoplePicker-persona");
+      this._addRemoveBtn(tokenResult);
+
       if (tokenResult.classList.contains("ms-Persona--sm")) {
         tokenResult.classList.remove("ms-Persona--sm");
         tokenResult.classList.add("ms-Persona--xs");
@@ -78,10 +86,27 @@ namespace fabric {
       return currentElement;
     }
 
+    private _addRemoveBtn(persona: Element) {
+      let actionBtn = document.createElement("div");
+      let actionIcon = document.createElement("i");
+
+      actionBtn.classList.add("ms-Persona-actionIcon");
+      actionIcon.classList.add("ms-Icon", "ms-Icon--x");
+
+      actionBtn.appendChild(actionIcon);
+
+      return persona.appendChild(actionBtn);
+    }
+
     private _assignClicks() {
       this._container.addEventListener("click", this._clickHandler.bind(this), true);
       this._container.addEventListener("focus", this._clickHandler.bind(this), true);
       this._container.addEventListener("keyup", this._clickHandler.bind(this), true);
+
+      // for (let i = 0; i < this._peoplePickerResults.length; i++) {
+      //   this._peoplePickerResults[i].addEventListener("click", this._selectResult.bind(this), true);
+      // }
+
     }
   }
 }

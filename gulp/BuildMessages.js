8800d0d6fab6a13108eef4182ee40b6a87cc6a53
Eddie Liu <v-edwliu@microsoft.com>
2016-08-18 10:23:24 -0700

    Basic functionality of localhost doc working. Todo:  removed unused functions and files, style navigation

--- gulp/BuildMessages.js
+++ gulp/BuildMessages.js
@@ -12,6 +12,10 @@ gulp.task('BuildMessages-finished', BuildConfig.buildTasks, function () {
     return console.log(ConsoleHelper.generateSuccess('Fabric build was successful, now sing and dance!', true));
 });
 
+gulp.task('BuildMessages-server', BuildConfig.buildTasks, function () {
+    return console.log(ConsoleHelper.generateSuccess('Fabric built successfully! ' + "\r\n" + 'Fabric documentation located at ' + Config.projectURL + ':' + Config.port, false));
+});
+
 gulp.task('BuildMessages-updated', BuildConfig.buildTasks, function () {
     return console.log(ConsoleHelper.generateSuccess('Fabric updated, yay!', false));
 });

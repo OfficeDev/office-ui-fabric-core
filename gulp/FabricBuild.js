fbabf38baa1507a3dc1bd4bf380ff11180c6a053
Peter Jahn <pejahn@microsoft.com>
2016-12-01 17:41:46 -0800

    Add template for Fabric.Scoped.scss, which will consume package.json version info at build time. Modify core Fabric build to accommodate scoped Fabric

--- gulp/FabricBuild.js
+++ gulp/FabricBuild.js
@@ -7,6 +7,15 @@ var BuildConfig = require('./modules/BuildConfig');
 var ConsoleHelper = require('./modules/ConsoleHelper');
 var ErrorHandling = require('./modules/ErrorHandling');
 var Plugins = require('./modules/Plugins');
+var pkg = require('../package.json');
+
+var versionParts = pkg.version.split('.');
+
+var version = {
+    major: versionParts[0],
+    minor: versionParts[1],
+    patch: versionParts[2]
+}
 
 //
 // Clean/Delete Tasks
@@ -14,7 +23,7 @@ var Plugins = require('./modules/Plugins');
 
 // Clean out the distribution folder.
 gulp.task('Fabric-nuke', function () {
-    return Plugins.del.sync([Config.paths.distCSS, Config.paths.distSass]);
+    return Plugins.del.sync([Config.paths.distCSS, Config.paths.distSass, Config.paths.temp]);
 });
 
 
@@ -24,7 +33,7 @@ gulp.task('Fabric-nuke', function () {
 
 // Copy all Sass files to distribution folder.
 gulp.task('Fabric-copyAssets', function () {            
-     var moveSass =  gulp.src([Config.paths.srcSass + '/**/*'])
+     var moveSass =  gulp.src([Config.paths.srcSass + '/**/*', !Config.paths.srcSass + '/Fabric.Scoped.scss'])
             .pipe(Plugins.plumber(ErrorHandling.onErrorInPipe))
             .pipe(Plugins.changed(Config.paths.distSass))
             .pipe(Plugins.gulpif(Config.debugMode, Plugins.debug({
@@ -39,6 +48,19 @@ gulp.task('Fabric-copyAssets', function () {
 // ----------------------------------------------------------------------------
 
 // Build Sass files for core Fabric into LTR and RTL CSS files.
+
+gulp.task('Fabric-scopedPreBuild', function () {
+    var fabricScopedPreBuild = gulp.src(Config.paths.srcTemplate + '/' + 'scoped-fabric-template.' + BuildConfig.fileExtension)
+            .pipe(Plugins.plumber(ErrorHandling.onErrorInPipe))
+            .pipe(Plugins.data(function () {
+              return { "version": version };
+            }))
+            .pipe(Plugins.template())
+            .pipe(Plugins.rename('_Fabric.Scoped.Temp.scss'))
+            .pipe(gulp.dest(Config.paths.temp));
+
+    return fabricScopedPreBuild;
+});
 
 gulp.task('Fabric-buildStyles', function () {
     var fabric = gulp.src(BuildConfig.srcPath + '/' + 'Fabric.' + BuildConfig.fileExtension)
@@ -65,16 +87,15 @@ gulp.task('Fabric-buildStyles', function () {
             .pipe(Plugins.header(Banners.getBannerTemplate(), Banners.getBannerData()))
             .pipe(Plugins.header(Banners.getCSSCopyRight(), Banners.getBannerData()))
             .pipe(gulp.dest(Config.paths.distCSS));    
-
+    
     var fabricScoped = gulp.src(BuildConfig.srcPath + '/' + 'Fabric.Scoped.' + BuildConfig.fileExtension)
-            .pipe(Plugins.plumber(ErrorHandling.onErrorInPipe))
             .pipe(Plugins.gulpif(Config.debugMode, Plugins.debug({
-              title: "Building Core Fabric " + BuildConfig.fileExtension + " File"
+              title: "Building Core Fabric Scoped " + BuildConfig.fileExtension + " File"
             })))
             .pipe(Plugins.header(Banners.getBannerTemplate(), Banners.getBannerData()))
             .pipe(Plugins.header(Banners.getCSSCopyRight(), Banners.getBannerData()))
             .pipe(BuildConfig.processorPlugin().on('error', BuildConfig.compileErrorHandler))
-            .pipe(Plugins.rename('fabric.scoped.css'))
+            .pipe(Plugins.rename('fabric-' + version.major + '.scoped.css'))
             .pipe(Plugins.changed(Config.paths.distCSS, {extension: '.css'}))
             .pipe(Plugins.autoprefixer({
               browsers: ['last 2 versions', 'ie >= 9'],
@@ -83,14 +104,14 @@ gulp.task('Fabric-buildStyles', function () {
             .pipe(Plugins.cssbeautify())
             .pipe(Plugins.csscomb())
             .pipe(gulp.dest(Config.paths.distCSS))
-            .pipe(Plugins.rename('fabric.scoped.min.css'))
+            .pipe(Plugins.rename('fabric-' + version.major + '.scoped.min.css'))
             .pipe(Plugins.cssMinify({
                 safe: true
             }))
             .pipe(Plugins.header(Banners.getBannerTemplate(), Banners.getBannerData()))
             .pipe(Plugins.header(Banners.getCSSCopyRight(), Banners.getBannerData()))
             .pipe(gulp.dest(Config.paths.distCSS));
-                
+
     // Build full and minified Fabric RTL CSS.
     var fabricRtl = gulp.src(BuildConfig.srcPath + '/' + 'Fabric.Rtl.' + BuildConfig.fileExtension)
             .pipe(Plugins.plumber(ErrorHandling.onErrorInPipe))
@@ -117,6 +138,7 @@ gulp.task('Fabric-buildStyles', function () {
             .pipe(Plugins.header(Banners.getBannerTemplate(), Banners.getBannerData()))
             .pipe(Plugins.header(Banners.getCSSCopyRight(), Banners.getBannerData()))
             .pipe(gulp.dest(Config.paths.distCSS));
+
     // Merge all current streams into one.
     return Plugins.mergeStream(fabric, fabricScoped, fabricRtl);
 });
@@ -125,6 +147,6 @@ gulp.task('Fabric-buildStyles', function () {
 // Rolled up Build tasks
 // ----------------------------------------------------------------------------
 
-gulp.task('Fabric', ['Fabric-copyAssets', 'Fabric-buildStyles']);
+gulp.task('Fabric', ['Fabric-copyAssets','Fabric-scopedPreBuild', 'Fabric-buildStyles']);
 BuildConfig.buildTasks.push('Fabric');
 BuildConfig.nukeTasks.push('Fabric-nuke');

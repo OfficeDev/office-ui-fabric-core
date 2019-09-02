c69949535d8b0578e73cc5de2fe1955cf550ffc9
maxwellred <maxwell.rediker@gmail.com>
2019-09-01 17:58:31 -0700

    Updated plugin dependencies and deleted unused ones. Alphabetized plugins for scannability.

--- gulp/modules/Plugins.js
+++ gulp/modules/Plugins.js
@@ -3,37 +3,35 @@
  * Class for accessing gulp and node plugins
  */
 var Plugins = function() {
-	this.colors = require('colors/safe');
-	this.del = require('del');
 	this.autoprefixer = require('gulp-autoprefixer');
-  	this.changed = require('gulp-changed');
-	this.handlebars = require('gulp-compile-handlebars');
-	this.flipper = require('gulp-css-flipper');
+	this.colors = require('colors/safe');
+	this.changed = require('gulp-changed');
 	this.cssbeautify = require('gulp-cssbeautify');
 	this.csscomb = require('./CssComb');
 	this.cssMinify = require('gulp-clean-css');
 	this.data = require('gulp-data');
+	this.del = require('del');
+	this.debug = require('gulp-debug');
 	this.fileinclude = require('gulp-file-include');
-	this.header = require('gulp-header');
 	this.gulpif = require('gulp-if');
+	this.handlebars = require('gulp-compile-handlebars');
+	this.header = require('gulp-header');
 	this.lintspaces = require('./LintSpaces');
-	this.nugetpack = require('gulp-nuget-pack');
+	this.log = require('fancy-log');
+	this.mergeStream = require('merge-stream');
+	this.nugetPack = require('gulp-nuget-pack');
+	this.pkg = require('../../package.json');
+	this.pluginError = require('plugin-error');
 	this.plumber = require('gulp-plumber');
 	this.rename = require('gulp-rename');
+	this.replace = require('gulp-replace');
+	this.requireDir = require('require-dir');
 	this.sass = require('gulp-sass');
 	this.size = require('gulp-size');
 	this.template = require('gulp-template');
-	this.tslint = require("gulp-tslint");
 	this.tsc = require('gulp-typescript');
-	this.gutil = require('gulp-util');
-	this.debug = require('gulp-debug');
-	this.mergeStream = require('merge-stream');
-	this.requireDir = require('require-dir');
+	this.tslint = require("gulp-tslint");
 	this.walkSync = require('walk-sync');
-	this.path = require('path');
-	this.pkg = require('../../package.json');
-	this.fs = require('fs');
-	this.replace = require('gulp-replace');
 };
 
 module.exports = new Plugins();
